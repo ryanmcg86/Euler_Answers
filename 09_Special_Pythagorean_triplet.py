b38427e5b014ab01972f627263711b5c90c242b3
Ryan <ryanmcg86@users.noreply.github.com>
2019-06-10 16:49:30 -0400

    Update 09_Special_Pythagorean_triplet.py

--- 09_Special_Pythagorean_triplet.py
+++ 09_Special_Pythagorean_triplet.py
@@ -15,12 +15,12 @@ def isPythag(a, b, c):
     return a**2 + b**2 == c**2
 
 #Build a findProd function
-def findProd():
+def findProd(num):
     start = time.time()
     triplet = []
     for a in range(1, num / 3):
         b = a + 1
-        c - num - a - b
+        c = num - a - b
         while a < b and b < c:
             if isPythag(a, b, c):
                 triplet = [a, b, c]
@@ -31,9 +31,10 @@ def findProd():
     prod = str(a * b * c)
     a, b, c = str(a), str(b), str(c)
     
-    print 'The Pythagorean triplet for which a + b + c = 1000 is ' + a + ', ' + b + ', and ' + c + '.'
+    print 'The Pythagorean triplet for which a + b + c = ' + str(num) + ' is ' + a + ', ' + b + ', and ' + c + '.'
     print 'The product of ' + str(a) + ' * ' + str(b) + ' * ' + str(c) + ' is ' + prod + '.'
     print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
     
 #Find the product of the Pythagorean triplet for which a + b + c = 1000
-findProd()
+num = 1000
+findProd(num)
