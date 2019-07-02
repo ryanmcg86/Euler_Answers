a0e21f14fa7daa10daf216f31c1b753519491a27
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-02 18:34:53 -0400

    Update 051_Prime_digit_replacements.py

--- 051_Prime_digit_replacements.py
+++ 051_Prime_digit_replacements.py
@@ -1,7 +1,19 @@
+'''The prime 41, can be written as the sum of six consecutive primes:
+41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+The longest sum of consecutive primes below one-thousand that adds to a prime, 
+contains 21 terms, and is equal to 953.
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
+Link: https://projecteuler.net/problem=50'''
+
+#Imports
+import time
+
+#Build an isPrime function
 def isPrime(n):
-    if n < 2:
+    if n <= 1:
         return False
-    if n < 4:
+    if n <= 3:
         return True
     if n % 2 == 0 or n % 3 == 0:
         return False
@@ -11,29 +23,33 @@ def isPrime(n):
             return False
         i += 6
     return True
-
+    
+#Build a sieve function
 def build_sieve(n):
     if n < 2:
         return []
     if n < 3:
         return [2]
     prime_sieve = [2, 3]
+    if n < 5:
+        return prime_sieve
     i = 5
     while i <= n:
         if isPrime(i):
             prime_sieve.append(i)
-        if i + 2 > n:
-            break
-        if isPrime(i + 2):
+        if isPrime(i + 2) and i + 2 <= n:
             prime_sieve.append(i + 2)
         i += 6
     return prime_sieve
 
+#Build a solve function
 def solve(n):
+    #Define variables
     start = time.time()
     primes = build_sieve(n)
     ans = 0
 	
+    #Solve the problem
     for prime in primes:
         string_prime = str(prime)
         for n in range(0, len(string_prime)):
@@ -49,5 +65,12 @@ def solve(n):
         if ans != 0:
             break
 
-    print ans
-    print str(time.time() - start)
+    #Print the results
+    print 'The smallest prime which, by replacing part of the number '
+    print '(not necessarily adjacent digits) with the same digit, '
+    print 'is part of an eight prime value family, is ' + ans + '.'
+    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+
+#Run the program
+n = 1000000
+solve(n)
