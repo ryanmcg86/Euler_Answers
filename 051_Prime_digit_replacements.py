0c7b6335a2d02a586f6de856aababfdae138ce67
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-03 01:01:03 -0400

    Update 051_Prime_digit_replacements.py

--- 051_Prime_digit_replacements.py
+++ 051_Prime_digit_replacements.py
@@ -1,9 +1,14 @@
-'''The prime 41, can be written as the sum of six consecutive primes:
-41 = 2 + 3 + 5 + 7 + 11 + 13
-This is the longest sum of consecutive primes that adds to a prime below one-hundred.
-The longest sum of consecutive primes below one-thousand that adds to a prime, 
-contains 21 terms, and is equal to 953.
-Which prime, below one-million, can be written as the sum of the most consecutive primes?
+'''By replacing the 1st digit of the 2-digit number *3, it turns out 
+that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+
+By replacing the 3rd and 4th digits of 56**3 with the same digit, 
+this 5-digit number is the first example having seven primes among the 
+ten generated numbers, yielding the family: 56003, 56113, 56333, 56443, 
+56663, 56773, and 56993. Consequently 56003, being the first member of this family, 
+is the smallest prime with this property.
+
+Find the smallest prime which, by replacing part of the number 
+(not necessarily adjacent digits) with the same digit, is part of an eight prime value family.
 Link: https://projecteuler.net/problem=51'''
 
 #Imports
