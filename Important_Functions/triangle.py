36d4421922a26450e232d9631275ecf7aa981a35
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-18 15:30:02 -0400

    Update and rename trianglenum.py to triangle.py

---
+++ Important_Functions/triangle.py
@@ -0,0 +1,4 @@
+'''This function returns the triangle of an inputted n'''
+
+def triangle(n):
+    return (n * (n + 1)) / 2
--- Important_Functions/trianglenum.py
+++
@@ -1,5 +0,0 @@
-'''This function finds the triangle
-number for n natural numbers'''
-
-def tri(n):
-    return n * (n + 1) / 2
