1dd117569af7b6097938e153ae85366597a74f6d
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-18 01:04:53 -0400

    Create 085_Counting_rectangles.py

---
+++ 085_Counting_rectangles.py
@@ -0,0 +1,50 @@
+'''By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles.
+
+
+Although there exists no rectangular grid that contains exactly two million rectangles, 
+find the area of the grid with the nearest solution.
+Link: https://projecteuler.net/problem=85'''
+
+#Imports
+import time
+
+#Build an amount function
+def amount(x, y):
+    return sum((x - i) * (y - j) for i in range(x) for j in range(y))
+
+#Build a solve function
+def solve(limit):
+    #Define variables
+    start = time.time()
+    i = 1
+    j = 1
+    I = 0
+    J = 0
+    closest = 999999
+    num = amount(i, j)
+
+    #Solve the problem
+    while num <= limit:
+        while num <= limit:
+            diff = abs(limit - num)
+            if diff < closest:
+                closest = diff
+                I = i
+                J = j
+            i += 1
+            num = amount(i, j)
+        j += 1
+        i = 1
+        num = amount(i, j)
+
+    ans = str(I * J)
+
+    #Print the results
+    print 'The area of the grid with the closest '
+    print 'amount of rectangles to ' + str(limit) + ' is ' + ans + '.' 
+    print 'It was a ' + str(I) + ' x ' + str(J) + ' grid which had the nearest solution.'
+    print 'This took ' + str(time.time() - start) + ' seconds too calculate.'
+
+#Run the program
+limit = 2 * 10**6
+solve(limit)
