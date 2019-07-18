17700a54c0cc9a8fe42adb014221ca31a4dc129f
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-18 12:14:34 -0400

    Create isPrime.py

---
+++ Important_Functions/isPrime.py
@@ -0,0 +1,12 @@
+'''This function returns whether a number is prime or not'''
+
+def isPrime(n):
+    if n < 2: return False
+    if n < 4: return True
+    if n % 2 == 0 or n % 3 == 0: return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
