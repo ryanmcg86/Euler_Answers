3f3b0d58e0b05e4e42a78bb3a8b9a12d8793de4e
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-18 13:28:09 -0400

    Update proper_divisors.py

--- Important_Functions/proper_divisors.py
+++ Important_Functions/proper_divisors.py
@@ -1,9 +1,9 @@
 '''This function returns a list of all of the proper divisors of a given input n'''
 
-def properDivisors(n):
-    divisors = [1]
+def pd(n):
+    pd = [1]
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            divisors.append(i)
-            divisors.append(n / i)
-    return list(sorted(set(divisors)))
+            pd.append(i)
+            pd.append(n / i)
+    return list(sorted(set(pd)))
