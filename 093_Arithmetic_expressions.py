035aaaa0b40dbfb0ec21d42cab527106caf0ab65
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-24 18:01:20 -0400

    Update and rename 093_.py to 093_Arithmetic_expressions.py

--- 093_Arithmetic_expressions.py
+++ 093_Arithmetic_expressions.py
@@ -1,12 +1,22 @@
-'''A number chain is created by continuously adding the square of the 
-digits in a number to form a new number until it has been seen before.
+'''By using each of the digits from the set, {1, 2, 3, 4}, exactly once, 
+and making use of the four arithmetic operations (+, −, *, /) and brackets/parentheses, 
+it is possible to form different positive integer targets.
+
 For example,
-44 → 32 → 13 → 10 → 1 → 1
-85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
-Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop. 
-What is most amazing is that EVERY starting number will eventually arrive at 1 or 89.
-How many starting numbers below ten million will arrive at 89?
-Link: https://projecteuler.net/problem=92'''
+
+8 = (4 * (1 + 3)) / 2
+14 = 4 * (3 + 1 / 2)
+19 = 4 * (2 + 3) − 1
+36 = 3 * 4 * (2 + 1)
+
+Note that concatenations of the digits, like 12 + 34, are not allowed.
+
+Using the set, {1, 2, 3, 4}, it is possible to obtain thirty-one different target numbers of which 36 is the maximum, 
+and each of the numbers 1 to 28 can be obtained before encountering the first non-expressible number.
+
+Find the set of four distinct digits, a < b < c < d, for which the longest set of consecutive positive integers, 
+1 to n, can be obtained, giving your answer as a string: abcd.
+Link: https://projecteuler.net/problem=93'''
 
 #Imports
 import time
