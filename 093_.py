ce6799ae333db3dc6702011488e11a124cca633a
Ryan <ryanmcg86@users.noreply.github.com>
2019-07-24 17:36:44 -0400

    Update 093_.py

--- 093_.py
+++ 093_.py
@@ -10,19 +10,36 @@ Link: https://projecteuler.net/problem=92'''
 
 #Imports
 import time
+from itertools import combinations as c, permutations as p, count as co, product as pr
+import operator as o
 
+#Build a length function
+def length(digits):
+    outputs = set()
+    ops = [o.add, o.sub, o.mul, o.truediv]
+    for a, b, c, d in p(digits):
+        for op1, op2, op3 in pr(ops, repeat = 3):
+            outputs.update([
+                op1(op2(op3(a, b), c), d), 
+                op1(op2(a, b), op3(c, d))
+            ])
+    return next(i for i in co(1) if i not in outputs) - 1
 
 #Build a solve function
 def solve():
     #Define variables
     start = time.time()
-    
+    ans = ''
 
-    #Solve the problem	
-    
+    #Solve the problem
+    nums = max(c(range(1, 10), 4), key = length)
+    for i in nums:
+        ans += str(i)
     
     #Print the results
-    
+    print 'The string that represents the concatenation of the 4 single '
+    print 'digit numbers where a < b < c < d, for which the longest set of '
+    print 'consecutive positive integers, 1 to n, can be obtained, is ' + ans + '.'
     print 'This took ' + str(time.time() - start) + ' seconds too calculate.'
 
 #Run the program
