b7ecc75e2017bae487fa2582f3b1c90f04901b88
Ryan <ryanmcg86@users.noreply.github.com>
2019-09-09 14:48:06 -0400

    Create fib.py

---
+++ Important_Functions/fib.py
@@ -0,0 +1,11 @@
+'''This is a O(log n) implementation of a function that retreives the nth number in Fibonacci's sequence.'''
+
+fibs = {0: 0, 1: 1}
+
+def fib(n):
+    if n in fibs: return fibs[n]
+    if n % 2 == 0:
+        fibs[n] = ((2 * fib((n / 2) - 1)) + fib(n / 2)) * fib(n / 2)
+    else:
+        fibs[n] = (fib((n - 1) / 2)**2) + (fib((n + 1) / 2)**2)
+    return fibs[n]
