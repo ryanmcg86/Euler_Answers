b09d20daf23ea89e6d3072a3922d68da9c1541c3
Ryan <ryanmcg86@users.noreply.github.com>
2019-09-13 17:26:25 -0400

    Update 164_Numbers_for_which_no_three_consecutive_digits_have_a_sum_greater_than_a_given_value.py

--- 164_Numbers_for_which_no_three_consecutive_digits_have_a_sum_greater_than_a_given_value.py
+++ 164_Numbers_for_which_no_three_consecutive_digits_have_a_sum_greater_than_a_given_value.py
@@ -9,33 +9,30 @@ import time
 def solve(n):
     #Define variables
     start = time.time()
-    count = {(a, b): 1 for a in range(10) for b in range(10)}
+    count = [[1 for a in range(10)] for b in range(10)]
     ans = 0
     
     #Solve the problem
     for i in range(n - 2):
-        tempCount = {}
-	for b in range(10):
-	    for a in range(10):
-                s = 0
-		for c in range(10):
-                    if a + b + c <= 9:
-                        s += count[(b, c)]
-                tempCount.update({(a, b): s})
-        count = tempCount
-		
-    for a in range(1, 10):
+        tempCount = [[0 for j in range(10)] for k in range(10)]
         for b in range(10):
-            ans += count[(a, b)]
-    ans = str(ans)
+            for a in range(10):
+                s = 0
+                for c in range(10):
+                    if a + b + c <= 9:
+                        s += count[b][c]
+                tempCount[a][b] = s
+        count = tempCount
+	
+    ans = str(sum(count[a][b] for a in range(1, 10) for b in range(10)))
     n = str(n)
-        
+
     #Print the results
     print 'There are ' + ans + ' ' + n + '-digit numbers n '
     print '(without any leading zero) which exist such that '
     print 'no 3 consecutive digits of n have a sum greater than 9.'
     print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
-    
+
 #Run the program
 n = 20
 solve(n)
