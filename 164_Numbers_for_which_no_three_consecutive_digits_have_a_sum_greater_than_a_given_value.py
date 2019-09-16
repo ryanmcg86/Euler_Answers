5247270265345d13e098a58324d2881e2273b81c
Ryan <ryanmcg86@users.noreply.github.com>
2019-09-16 10:23:21 -0400

    Update 164_Numbers_for_which_no_three_consecutive_digits_have_a_sum_greater_than_a_given_value.py

--- 164_Numbers_for_which_no_three_consecutive_digits_have_a_sum_greater_than_a_given_value.py
+++ 164_Numbers_for_which_no_three_consecutive_digits_have_a_sum_greater_than_a_given_value.py
@@ -6,7 +6,7 @@ Link: https://projecteuler.net/problem=164'''
 import time
    
 #Build a Solve function
-def solve(n):
+def solve(n, nmax):
     #Define variables
     start = time.time()
     count = [[1 for a in range(10)] for b in range(10)]
@@ -19,20 +19,22 @@ def solve(n):
             for a in range(10):
                 s = 0
                 for c in range(10):
-                    if a + b + c <= 9:
+                    if a + b + c <= nmax:
                         s += count[b][c]
                 tempCount[a][b] = s
         count = tempCount
 	
     ans = str(sum(count[a][b] for a in range(1, 10) for b in range(10)))
     n = str(n)
+    mx = str(nmax)
 
     #Print the results
     print 'There are ' + ans + ' ' + n + '-digit numbers n '
     print '(without any leading zero) which exist such that '
-    print 'no 3 consecutive digits of n have a sum greater than 9.'
+    print 'no 3 consecutive digits of n have a sum greater than ' + mx + '.'
     print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
 
 #Run the program
 n = 20
-solve(n)
+nmax = 9
+solve(n, nmax)
