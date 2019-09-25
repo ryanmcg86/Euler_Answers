a2a6c40cefb77ee1b6acfaf08a6effec49f0dc57
Ryan <ryanmcg86@users.noreply.github.com>
2019-09-25 12:19:59 -0400

    Create 180_.py

---
+++ 180_.py
@@ -0,0 +1,21 @@
+'''
+Link: https://projecteuler.net/problem=178'''
+
+#Imports
+import time
+
+#Build a Solve function
+def solve():
+    #Define variables
+    start = time.time()
+    
+
+    #Solve the problem
+    
+        
+    #Print the results
+
+    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+
+#Run the program
+solve()
