e88e88c65a0171819d7caf19cff293a4cb2c38c1
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-16 12:15:14 -0400

    Update SieveOfEratosthenes.py

--- Important_Functions/SieveOfEratosthenes.py
+++ Important_Functions/SieveOfEratosthenes.py
@@ -1,18 +1,14 @@
 # Python program to output all primes smaller than or equal to 
 # n using Sieve of Eratosthenes 
 
-def SieveOfEratosthenes(n): 
-    prime = [True for i in range(n + 1)]
-    ans = []
-    p = 2
-    while (p * p <= n): 
-        if (prime[p] == True): 
+def SoE(n): 
+    prime = [False] * 2 + [True for i in range(n - 1)]
+    ans, p = [], 2
+    while p**2 <= n: 
+        if prime[p]: 
             for i in range(p * 2, n + 1, p): 
                 prime[i] = False
         p += 1
-    prime[0] = False
-    prime[1] = False
     for p in range(n + 1): 
-        if prime[p]: 
-            ans.append(p)
+        if prime[p]: ans.append(p)
     return ans
