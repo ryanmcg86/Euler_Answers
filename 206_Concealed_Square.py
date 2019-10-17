1ff6f41050fad581aef61a432d0e4218a0923798
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-17 12:37:12 -0400

    Create 206_Concealed_Square.py

---
+++ 206_Concealed_Square.py
@@ -0,0 +1,32 @@
+'''Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
+where each “_” is a single digit.
+Link: https://projecteuler.net/problem=206'''
+
+#Imports
+import time
+
+#Build a match function
+def match(n):
+    s = str(n)
+    return not all(int(s[x * 2]) == x + 1 for x in range(9))
+
+#Build a Solve function
+def solve():
+    #Define variables
+    start = time.time()
+    n = int(19293949596979899**0.5) + 1
+
+    #Solve the problem
+    while match(n**2):
+        n -= 2
+        
+    ans = str(n * 10)
+
+    #Print the results
+    print('The unique positive integer whose square ')
+    print('has the form 1_2_3_4_5_6_7_8_9_0, where each ')
+    print('"_" is a single digit, is ' + ans + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
+
+#Run the program
+solve()
