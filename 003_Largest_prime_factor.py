310021032aa1186019f952e40f13cc376b3fab51
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-21 13:15:59 -0400

    Update 003_Largest_prime_factor.py

--- 003_Largest_prime_factor.py
+++ 003_Largest_prime_factor.py
@@ -6,35 +6,34 @@ Link: https://projecteuler.net/problem=3'''
 #Imports
 import time
 
-#isPrime function
-def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    while i * i <= n:
-        if n % i == 0 or n % (n + 2) == 0:
-            return False
-        i += 6
-    return True
+#Build a factors function
+def factors(n):
+    factors = []
+    for i in range(2, 4):
+        while n % i == 0:
+            factors.append(i)
+            n /= i
+    for i in range(5, int(n**0.5) + 1, 6):
+        plus2 = [i, i + 2]
+        for j in plus2:
+            while n % j == 0:
+                factors.append(j)
+                n /= j
+    if n > 2:
+        factors.append(n)
+    return factors
 
 #Largest prime factor function
 def lpf(n):
+    #Declare variables
     start = time.time()
-    result = 1
-    while n % 2 == 0:
-        n /= 2
-        result = n
-    begin = int(n**0.5)
-    if begin % 2 == 0:
-        begin -= 1
-    for i in range(begin, 1, -2):
-        if isPrime(i) and n % i == 0:
-            result = i
-            break
-    print 'The largest prime factor of ' + str(n) + ' is ' + str(result) + '.'
+    
+    #Solve the problem
+    ans = str(factors(n)[-1])
+    n = str(n)
+    
+    #Print the results
+    print 'The largest prime factor of ' + n + ' is ' + ans + '.'
     print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
     
 #Run the program
