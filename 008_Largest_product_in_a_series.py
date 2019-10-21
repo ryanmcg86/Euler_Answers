396df7c181da1a8e2061186efa61108f44d4d855
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-21 15:03:41 -0400

    Update 008_Largest_product_in_a_series.py

--- 008_Largest_product_in_a_series.py
+++ 008_Largest_product_in_a_series.py
@@ -26,27 +26,28 @@ Link: https://projecteuler.net/problem=8'''
 
 #Imports
 import time
+from math import prod
 
 #Build a find-the-maximum-product function
 def findMaxProd(num, length):
     start = time.time()
     index = 0
     maxProd = 0
-    for i in range(0, len(num) - length):
-        prod = 1
-        for j in range(i, i + length):
-            prod *= int(num[j])
-        if prod > maxProd:
-            maxProd = prod
+    for i in range(len(num) - length):
+        p = prod(int(num[j]) for j in range(i, i + length))
+        if p > maxProd:
+            maxProd = p
             index = i
     
     ad = num[index:index + length]
+    l = str(length)
+    mp = str(maxProd)
     
-    print 'The largest product made up of ' + str(length) + ' adjacent digits is ' + str(maxProd) + '.'
-    print 'The ' + str(length) + ' adjacent digits are: ' + ad + '.'
-    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+    print('The largest product made up of ' + l + ' adjacent digits is ')
+    print(mp + '. The ' + l + ' adjacent digits are: ' + ad + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
     
 #Find the 10001st prime
 num = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450'
 length = 13
-findMaxProd(num, 13)
+findMaxProd(num, length)
