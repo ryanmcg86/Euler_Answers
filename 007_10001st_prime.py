333b9d23c5dcbc3360a64667498cc1e2787efbef
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-21 14:50:49 -0400

    Update 007_10001st_prime.py

--- 007_10001st_prime.py
+++ 007_10001st_prime.py
@@ -8,26 +8,21 @@ import time
 
 #Build a suffix function
 def buildSuffix(num):
-    suff = 'th'
+    suffs = ['th', 'st', 'nd', 'rd']
+    suff = suffs[0]
     begin = len(str(num)) - 2
     end = begin + 1
     if str(num)[begin:end] != '1':
-        if   int(str(num)[-1]) == 1:
-            suff = 'st'
-        elif int(str(num)[-1]) == 2:
-            suff = 'nd'
-        elif int(str(num)[-1]) == 3:
-            suff = 'rd'
+        lastdigit = int(str(num)[-1])
+        if lastdigit < len(suffs):
+            suff = suffs[lastdigit]
     return suff
 
 #Build an isPrime function
 def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
+    if n < 2: return False
+    if n < 4: return True
+    if n % 2 == 0 or n % 3 == 0: return False
     i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
@@ -35,22 +30,38 @@ def isPrime(n):
         i += 6
     return True
 
-#nthPrime function
+#Build an nthPrime function
 def nthPrime(n):
-    start = time.time()
-    count = 2
-    i = 5
+    if n < 1: return 'Error'
+    if n == 1: return 2
+    if n == 2: return 3
+    count, i, p = 2, 5, 5
     while count < n:
         if isPrime(i):
             count += 1
-        if count < n:
-            i += 2
+            p = i
+        if count == n: break
+        if isPrime(i + 2):
+            count += 1
+            p = i + 2
+        i += 6
+    return p
+
+#nthPrime function
+def solve(n):
+    #Define variables
+    start = time.time()
+    
+    #Solve the problem
+    ans = str(nthPrime(n))
     
     suff = buildSuffix(n)
+    n = str(n)
     
-    print 'The ' + str(n) + suff + ' prime number is ' + str(i) + '.'
-    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+    #Print the results
+    print('The ' + n + suff + ' prime number is ' + str(ans) + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
     
 #Find the 10001st prime
 n = 10001
-nthPrime(n)
+solve(n)
