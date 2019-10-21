d901adb8dbb497f0954dee6e35221eec801dd85d
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-21 14:16:52 -0400

    Update 005_Smallest_multiple.py

--- 005_Smallest_multiple.py
+++ 005_Smallest_multiple.py
@@ -5,6 +5,7 @@ Link: https://projecteuler.net/problem=5'''
 
 #Imports
 import time
+from functools import reduce
 
 #Lowest Common Multiple function
 def lcm(a, b):
@@ -13,16 +14,22 @@ def lcm(a, b):
         a, b = b, a % b
     return a1 * b1 // a
 
+#lcm function for more than 2 numbers
+def lcms(n):
+    return reduce(lcm, [i for i in range(1, n + 1)])
+
 #Run function
 def run(n):
+    #Declare variables
     start = time.time()
-    ans = 1
-    for i in range(1, n + 1):
-        ans = lcm(ans, i)
+    
+    #Solve the problem
+    ans = str(lcms(n))
             
-    print 'The smallest positive number that is evenly divisible by all of '
-    print 'the numbers from 1 to ' + str(n) + '  is ' + str(ans) + '.'
-    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+    #Print the results
+    print('The smallest positive number that is evenly divisible ')
+    print('by all of the numbers from 1 to ' + str(n) + ' is ' + ans + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
     
 #Run the program
 n = 20
