ccf64fb0031408451a08283d2e131ee58267099e
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-21 15:12:19 -0400

    Update 009_Special_Pythagorean_triplet.py

--- 009_Special_Pythagorean_triplet.py
+++ 009_Special_Pythagorean_triplet.py
@@ -15,10 +15,14 @@ def isPythag(a, b, c):
     return a**2 + b**2 == c**2
 
 #Build a findProd function
-def findProd(num):
+def findProd():
+    #Define variables
     start = time.time()
+    num = 1000
     triplet = []
-    for a in range(1, num / 3):
+    
+    #Solve the problem
+    for a in range(1, int(num / 3)):
         b = a + 1
         c = num - a - b
         while a < b and b < c:
@@ -27,14 +31,17 @@ def findProd(num):
                 break
             b += 1
             c = num - a - b
+            
     a, b, c = triplet[0], triplet[1], triplet[2]
     prod = str(a * b * c)
     a, b, c = str(a), str(b), str(c)
+    p = a + ' * ' + b + ' * ' + c
+    n = str(num)
     
-    print 'The Pythagorean triplet for which a + b + c = ' + str(num) + ' is ' + a + ', ' + b + ', and ' + c + '.'
-    print 'The product of ' + str(a) + ' * ' + str(b) + ' * ' + str(c) + ' is ' + prod + '.'
-    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+    print('The Pythagorean triplet for which ')
+    print('a + b + c = 1000 is ' + a + ', ' + b + ', and ' + c + '.')
+    print('The product of ' + p + ' is ' + prod + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
     
 #Find the product of the Pythagorean triplet for which a + b + c = 1000
-num = 1000
-findProd(num)
+findProd()
