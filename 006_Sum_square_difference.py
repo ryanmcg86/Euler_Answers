53a4509e75dc603da156b42a247a48f50308ce48
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-25 15:51:01 -0400

    Update 006_Sum_square_difference.py

--- 006_Sum_square_difference.py
+++ 006_Sum_square_difference.py
@@ -16,7 +16,7 @@ import time
 
 #Sum of squares function
 def sumOfsquares(n):
-    return sum(i**2 for i in range(1, n + 1))
+    return n * (n + 1) * (2 * n + 1) / 6
     
 #Square of sum function
 def squareOfsum(n):
