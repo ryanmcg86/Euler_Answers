d97a8a7b231235e6b4fe482f58a67a64548dbead
Ryan <ryanmcg86@users.noreply.github.com>
2019-10-25 13:05:44 -0400

    Update 001_Multiples_of_3_and_5.py

--- 001_Multiples_of_3_and_5.py
+++ 001_Multiples_of_3_and_5.py
@@ -6,6 +6,22 @@ Link: https://projecteuler.net/problem=1'''
 
 #Imports
 import time
+from math import prod
+from itertools import combinations as co
+
+#Build a Sum of Multiples function
+def SoM(num, mul):
+    n = (num - 1) // mul
+    return (n * (n + 1) * mul) >> 1 #divide by 2 without rounding error
+
+#Build an inclusion-exclusion function
+def inex(n, mults):
+    num = -1
+    ans = 0
+    for i in range(1, len(mults) + 1):
+        for j in co(mults, i):
+            ans += num**(i + 1) * SoM(n, prod(list(j)))
+    return ans
 
 #Sum of multiples (of 3 and 5) function
 def SumOfMults(n, mults):
@@ -17,22 +33,21 @@ def SumOfMults(n, mults):
     #Solve the problem
     for i in range(len(mults)):
         if i != len(mults) - 1:
-            strnums += str(mults[i]) + ', '
+            strnums += str(list(mults)[i]) + ', '
         else:
             strnums = strnums[:-2]
-            strnums += ' and ' + str(mults[i])
-        for j in range(0, n, mults[i]):
-            multiples.add(j)
-    
-    ans = str(sum(multiples))
+            strnums += ' and ' + str(list(mults)[i])
+            
+    if len(mults) == 1: strnums = str(list(mults)[0])
+    ans = str(inex(n, mults))
     n = str(n)
             
     #Print the results
-    print('The sum of all the multiples of ' + strnums)
-    print('below ' + n + ' is ' + ans + '.')
+    print('The sum of all of the multiples of ')
+    print(strnums + ' below ' + n + ' is ' + ans + '.')
     print('This took ' + str(time.time() - start) + ' seconds to calculate.')
 
 #Run the program
 n = 1000
-mults = [3, 5]
+mults = {3, 5}
 SumOfMults(n, mults)
