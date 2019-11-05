7238ce0feda59adb5ed5ab3a4e9242e0254e6c3c
Ryan <ryanmcg86@users.noreply.github.com>
2019-11-05 13:45:23 -0500

    Update 027_Quadratic_primes.py

--- 027_Quadratic_primes.py
+++ 027_Quadratic_primes.py
@@ -23,18 +23,15 @@ import time
     
 #Build an isPrime function
 def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
+    if n < 2: return False
+    if n < 4: return True
+    if n % 2 == 0 or n % 3 == 0: return False
     i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
             return False
         i += 6
-    return True
+    return Tru
 
 #Build a function that returns the product
 #of the coefficients, a and b, for the quadratic
