afae1c4f55084da88c73cd9fe75ad92260fe4049
Ryan <ryanmcg86@users.noreply.github.com>
2019-11-11 13:39:32 -0500

    Update MillerRabin.py

--- Important_Functions/MillerRabin.py
+++ Important_Functions/MillerRabin.py
@@ -1,6 +1,6 @@
 '''The following code finds whether big numbers are prime or not efficiently'''
 
-def isPrime(n):
+def isPrime_dumb(n):
     if n < 2: return False
     if n < 4: return True
     if n % 2 == 0 or n % 3 == 0: return False
@@ -11,18 +11,18 @@ def isPrime(n):
         i += 6
     return True
     
-def build_sieve(n):
-    if n < 2: return []
-    if n < 3: return [2]
-    ps = [2, 3]
-    i = 5
-    while i <= n:
-        if isPrime(i): ps.append(i)
-        m = i + 2
-        if m > n: break
-        if isPrime(m): ps.append(m)
-        i += 6
-    return ps
+#Build a Sieve of Eratosthenes
+def SoE(n): 
+    prime = [False] * 2 + [True for i in range(n - 1)]
+    ans, p = [], 2
+    while p**2 <= n: 
+        if prime[p]: 
+            for i in range(p * 2, n + 1, p): 
+                prime[i] = False
+        p += 1
+    for p in range(n + 1): 
+        if prime[p]: ans.append(p)
+    return ans
     
 def try_comp(a, d, n, s):
 	if pow(a, d, n) == 1: return False
@@ -32,7 +32,7 @@ def try_comp(a, d, n, s):
   
 def MillerRabin(n, prec):
     d, s = n - 1, 0
-    ps = build_sieve(prec)
+    ps = SoE(prec)
     maxes = [1373653, 25326001, 118670087467, 2152302898747, 3474749660383, 341550071728321]
     while not d % 2:
         d, s = d >> 1, s + 1
@@ -42,3 +42,9 @@ def MillerRabin(n, prec):
             if i == 2 and n == 3215031751: return False
             return not any(try_comp(a, d, n, s) for a in ps[:x])
     return not any(try_comp(a, d, n, s) for a in ps)
+
+def isPrime(n):
+	if n < 10000:
+		return isPrime_dumb(n)
+	else:
+		return MillerRabin(n, 25)
