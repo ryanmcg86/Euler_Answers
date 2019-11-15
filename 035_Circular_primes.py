2d4b117ac2824536a3fd18920c54c2bf2e3ad035
Ryan <ryanmcg86@users.noreply.github.com>
2019-11-15 16:33:04 -0500

    Update 035_Circular_primes.py

--- 035_Circular_primes.py
+++ 035_Circular_primes.py
@@ -10,12 +10,9 @@ import time
 
 #Build an isPrime function
 def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
+    if n < 2: return False
+    if n < 4: return True
+    if n % 2 == 0 or n % 3 == 0: return False
     i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
@@ -24,22 +21,17 @@ def isPrime(n):
     return True
 
 #Build a build-a-prime-sieve function
-def build_sieve(n):
-    if n < 2:
-        return []
-    if n < 3:
-        return [2]
-    prime_sieve = [2, 3]
-    if n < 5:
-        return prime_sieve
-    i = 5
-    while i <= n:
-        if isPrime(i):
-            prime_sieve.append(i)
-        if isPrime(i + 2):
-            prime_sieve.append(i + 2)
-        i += 6
-    return prime_sieve
+def SoE(n): 
+    prime = [False] * 2 + [True for i in range(n - 1)]
+    ans, p = [], 2
+    while p**2 <= n: 
+        if prime[p]: 
+            for i in range(p * 2, n + 1, p): 
+                prime[i] = False
+        p += 1
+    for p in range(n + 1): 
+        if prime[p]: ans.append(p)
+    return ans
     
 #Build an isCircular function
 def isCircular(n):
@@ -59,7 +51,7 @@ def isCircular(n):
 def Solve(num):
     start = time.time()
     
-    sieve = build_sieve(num)
+    sieve = SoE(num)
     count = 0
     for prime in sieve:
         if isCircular(prime):
