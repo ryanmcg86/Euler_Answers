824d701f8303894e2595774ba396648ac407279b
Ryan <ryanmcg86@users.noreply.github.com>
2019-11-20 17:26:52 -0500

    Update 043_Sub-string_divisibility.py

--- 043_Sub-string_divisibility.py
+++ 043_Sub-string_divisibility.py
@@ -18,14 +18,11 @@ Link: https://projecteuler.net/problem=43'''
 import time
 
 #Build a solve function
-def solve():
+def solve9():
 	#Define variables
 	start       = time.time()
-	total       = 0
 	digits      = {str(i) for i in range(10)}
-	#Include 1 with the primes, as we have
-	#to add a leading digit 7 total times.
-	primes      = [13, 11, 7, 5, 3, 2, 1]
+	primes      = [13, 11, 7, 5, 3, 2, 1]  #Include 1 with the primes, as we have to add a leading digit 7 total times.
 	pandigitals = []
     
         #Start with the 3 digit numbers divisible by 17,
@@ -60,4 +57,58 @@ def solve():
 	print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
 
 #Run the program
-solve()
+solve9()
+#----------------------------------Hackerrank Version----------------------------------#
+#Imports
+import time
+
+#Define a next_permutation function
+def next_permutation(a):
+    i = len(a) - 2
+    while not (i < 0 or a[i] < a[i + 1]): i -= 1
+    if i < 0: return False
+    j = len(a) - 1
+    while not (a[j] > a[i]): j -= 1
+    a[i], a[j] = a[j], a[i]
+    a[i + 1:] = reversed(a[i + 1:])
+    return True
+
+#Define a main function
+def main(n = 9):
+    #Define variables
+    v = [i for i in range(n + 1)]
+    s = 0
+    p = [2, 3, 5, 7, 11, 13, 17]
+	
+    #Solve the problem
+    if n == 9: return solve9()
+    while True:
+        sat = True
+        num = 10 * v[1] + v[2]
+        for ind in range(3, n + 1):
+            num = num % 100
+            num = num * 10 + v[ind]
+            if num % p[ind - 3] != 0:
+                sat = False
+                break
+        if sat:
+            s += int(''.join(str(i) for i in v))
+        if next_permutation(v) == False: break
+		
+    #Return the result
+    return s
+
+print(main(int(input())))
+
+#Don't run this in Hackerrank, but to formalize the answer and time it:
+def solve(n):
+    start = time.time()
+    ans, n = str(main(n)), str(n)
+    print('The sum of all 0 to ' + n + ' pandigital numbers ')
+    print('with the given property is ' + ans + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
+
+#Print all the results
+for i in range(3, 10):
+    print('')
+    solve(i)
