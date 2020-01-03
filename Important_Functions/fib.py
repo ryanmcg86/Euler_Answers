4bcc2ae873af514426e3227f010fe573c00c703c
Ryan <ryanmcg86@users.noreply.github.com>
2020-01-03 16:18:57 -0500

    Update fib.py

--- Important_Functions/fib.py
+++ Important_Functions/fib.py
@@ -7,5 +7,5 @@ def fib(n):
     if n % 2 == 0:
         fibs[n] = ((2 * fib((n / 2) - 1)) + fib(n / 2)) * fib(n / 2)
     else:
-        fibs[n] = (fib((n - 1) / 2)**2) + (fib((n + 1) / 2)**2)
+        fibs[n] = fib((n - 1) / 2)**2 + fib((n + 1) / 2)**2
     return fibs[n]
