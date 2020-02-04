edd9cab96a1f5346745bf899b7b20de22fe96b96
Ryan <ryanmcg86@users.noreply.github.com>
2020-02-04 16:08:00 -0500

    Update 039_Integer_right_triangles.py

--- 039_Integer_right_triangles.py
+++ 039_Integer_right_triangles.py
@@ -10,29 +10,22 @@ Link: https://projecteuler.net/problem=39'''
 import time
 
 #Build a Solve function
-def Solve(num):
-    start = time.time()
+def solve(n):
+    #Declare variables
+    start, p = time.time(), [0] * (n + 1)
     
-    perimeters = [0] * (num + 1)
-    
-    for a in range(1, num / 3 + 1):
-        for b in range(a + 1, num / 2):
+    #Solve the problem
+    for a in range(1, n // 3 + 1):
+        for b in range(a + 1, n // 2):
             c = int((a**2 + b**2)**0.5)
-            if a**2 + b**2 == c**2 and a + b + c <= num:
-                perimeters[a + b + c] += 1
-                
-    per = 0
+            if a**2 + b**2 == c**2 and a + b + c <= n:
+                p[a + b + c] += 1
     
-    for i in range(0, len(perimeters)):
-        if perimeters[i] == max(perimeters):
-            per = i
-            break
-        
-    per = str(per)
-    
-    print 'The value for which p <= ' + str(num) + ' is maximized is ' + per + '.'
-    print 'This took ' + str(time.time() - start) + ' seconds to calculate.'
+    #Print the results
+    print('The value for which p <= ' + str(n) + ' ')
+    print('is maximized is ' + str(p.index(max(p))) + '.')
+    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
     
 #Run the program
 num = 1000
-Solve(num)
+solve(num)
