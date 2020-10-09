6c8243210593507ac347451eb25ca4eb99d6ad80
Ryan <ryanmcg86@users.noreply.github.com>
2020-10-09 12:41:56 -0400

    Update 087_Prime_power_triples.py

--- 087_Prime_power_triples.py
+++ 087_Prime_power_triples.py
@@ -11,63 +11,48 @@ Link: https://projecteuler.net/problem=87'''
 
 #Imports
 import time
-
-#Build an isPrime function
-def isPrime(n):
-    if n < 2: 
-        return False
-    if n < 4: 
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
     
 #Build a prime sieve function
-def build_sieve(n):
-    if n < 2: return []
-    if n < 3: return [2]
-    ps = [2, 3]
-    i = 5
-    while i <= n:
-        if isPrime(i): ps.append(i)
-        m = i + 2
-        if m > n: break
-        if isPrime(m): ps.append(m)
-        i += 6
-    return ps
+def SoE(n):
+	prime = [False] * 2 + [True for i in range(n - 1)]
+	ans, p = [], 2
+	while p**2 <= n:
+		if prime[p]:
+			for i in range(p * 2, n + 1, p):
+				prime[i] = False
+		p += 1
+	return [i for i in range(n) if prime[i]]
 
 #Build a solve function
 def solve(limit):
     #Define variables
     start = time.time()
-    p = []
-    for i in range(2, 5):
-        p.append(build_sieve(int(limit**(1 / float(i))) + 1))
-    nums = set()
+    primes, ans = [], set()
     
-    #Update sieves with the correct exponents
-    for i in range(0, 3):
-        p[i] = [p[i][j]**(i + 2) for j in range(len(p[i]))]
+    #Update sieves with the primes raised to the correct exponent
+    for i in range(4, 1, -1):
+        primes.append(SoE(int(limit**(1 / float(i)))))
+        for j in range(len(primes[-1])):
+            primes[-1][j] = primes[-1][j]**i
 
     #Solve the problem
-    for i in p[0]:
-        for j in p[1]:
-            for k in p[2]:
-                num = i + j + k
+    for i in primes[0]:
+        for j in primes[1]:
+            ij = i + j
+            if ij >= limit: break
+            for k in primes[2]:
+                num = ij + k
                 if num < limit:
                     nums.add(num)
+                else: break
                     
-    ans = str(len(nums))
+    ans, lim = str(len(ans)), str(limit)
 
     #Print the results
-    print 'The amount of numbers that can be expressed as the sum of a prime square, '
-    print 'prime cube, and prime fourth power while being less than ' + str(limit) + ' is ' + ans + '.'
-    print 'This took ' + str(time.time() - start) + ' seconds too calculate.'
+	print('The amount of numbers that can be expressed ')
+	print('as the sum of a prime square, prime cube, and ')
+	print('prime fourth power while being less than ' + lim + ' is ' + ans + '.')
+	print('This took ' + str(time.time() - start) + ' seconds too calculate.')
 
 #Run the program
 limit = 5 * 10**7
