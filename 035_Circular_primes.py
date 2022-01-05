71d6d5e7281eacc9270f133eb2593bb712b4388c
Ryan <ryanmcg86@users.noreply.github.com>
2022-01-04 19:52:56 -0500

    Update 035_Circular_primes.py

--- 035_Circular_primes.py
+++ 035_Circular_primes.py
@@ -45,22 +45,42 @@ def isCircular(n):
         if not prime: return False
     return True
 
+#Build a function that simplifys the sieve
+#so it only has numbers that don't include
+#the digits 0, 2, 4, 5, 6, or 8, as none
+#of those can be circular.
+def simplify(sieve):
+    p = [2, 3, 5, 7]
+    for i in range(4, len(sieve)):
+        cir, pi = True, sieve[i]
+        s = str(pi)
+        a, b, c = s.find('0'), s.find('2'), s.find('4')
+        d, e, f = s.find('5'), s.find('6'), s.find('8')
+        for j in [a, b, c, d, e, f]:
+            if j != 1:
+                cir = False
+                break;
+        if cir:
+            p.append(pi)
+    return p
+
 #Build a Solve function
-def Solve(num):
+def solve(num):
     #Define variables
-    start = time.time()
+    st = time.time()
     sieve, count = SoE(num), 0
+    p = simplify(sieve)
     
     #Solve the problem
-    for prime in sieve:
+    for prime in p:
         if isCircular(prime): count += 1
         
-    count, num = str(count), str(num)
+    c, num = str(count), str(num)
     
     #Print the results
-    print('There are ' + count + ' circular primes below ' + num + '.')
-    print('This took ' + str(time.time() - start) + ' seconds to calculate.')
+    print('There are ' + c + ' circular primes below ' + num + '.')
+    print('This took ' + str(time.time() - st) + ' seconds to calculate.')
 
 #Run the program
 num = 10**6
-Solve(num)
+solve(num)
